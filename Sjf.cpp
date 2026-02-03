3fd2183ab85cde2d484240bf19a83c0a1eef8521
QuocPhu06 <phuphuu123qwe@gmail.com>
2026-02-03 19:22:27 +0700

    Update Sjf.cpp

--- Sjf.cpp
+++ Sjf.cpp
@@ -1,22 +1,14 @@
-#include <iostream>
 #include <vector>
+#include "process.h"
 using namespace std;
-struct Process {
-    int pid;
-    int arrival;
-    int burst;
-    int start;
-    int completion;
-    int waiting;
-    int turnaround;
-};
 void SJF(vector<Process>& p) {
-    int time = 0, completed = 0;
     int n = p.size();
+    int time = 0, completed = 0;
     vector<bool> done(n, false);
     while (completed < n) {
         int idx = -1;
         int minBurst = 1e9;
+        // LOGIC CHỌN PROCESS THEO BURST TIME
         for (int i = 0; i < n; i++) {
             if (!done[i] && p[i].arrival <= time && p[i].burst < minBurst) {
                 minBurst = p[i].burst;
@@ -36,24 +28,4 @@ void SJF(vector<Process>& p) {
         completed++;
     }
 }
-int main() {
-    vector<Process> p;
-    Process p1 = {1, 0, 5, 0, 0, 0, 0};
-    Process p2 = {2, 1, 3, 0, 0, 0, 0};
-    Process p3 = {3, 2, 8, 0, 0, 0, 0};
-    p.push_back(p1);
-    p.push_back(p2);
-    p.push_back(p3);
-    SJF(p);
-    cout << "PID\tAT\tBT\tST\tCT\tWT\tTAT\n";
-    for (int i = 0; i < p.size(); i++) {
-        cout << p[i].pid << "\t"
-             << p[i].arrival << "\t"
-             << p[i].burst << "\t"
-             << p[i].start << "\t"
-             << p[i].completion << "\t"
-             << p[i].waiting << "\t"
-             << p[i].turnaround << endl;
-    }
-    return 0;
-}
+
